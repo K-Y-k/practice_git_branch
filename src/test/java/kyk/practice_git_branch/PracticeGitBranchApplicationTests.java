0f8276ca9c416d8bf0b00c9a5d9e2a98a24d9f87
Kim-Yong-Kyeong <kyk4957@naver.com>
2024-04-03 00:31:31 +0900

    first commit

---
+++ src/test/java/kyk/practice_git_branch/PracticeGitBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package kyk.practice_git_branch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGitBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
