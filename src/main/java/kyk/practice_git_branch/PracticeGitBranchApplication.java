0a805779201527d18dac90982a188b48f518502a
Kim-Yong-Kyeong <kyk4957@naver.com>
2024-04-03 01:21:39 +0900

    Feature-A 구현 완료

--- src/main/java/kyk/practice_git_branch/PracticeGitBranchApplication.java
+++ src/main/java/kyk/practice_git_branch/PracticeGitBranchApplication.java
@@ -8,6 +8,8 @@ public class PracticeGitBranchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeGitBranchApplication.class, args);
+
+		System.out.println("테스트");
 	}
 
 }
